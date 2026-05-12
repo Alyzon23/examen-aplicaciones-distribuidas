9454860d50eb576e26d142812ff7030bb79d0218
alyzonegas2003 <alyzonegas2003@gmail.com>
2026-05-11 22:08:05 -0500

    update: eureka client config and spring boot downgrade

--- src/main/java/com/unibe/Examen/Aplicaciones/ExamenAplicacionesApplication.java
+++ src/main/java/com/unibe/Examen/Aplicaciones/ExamenAplicacionesApplication.java
@@ -2,8 +2,10 @@ package com.unibe.Examen.Aplicaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ExamenAplicacionesApplication {
 
 	public static void main(String[] args) {
